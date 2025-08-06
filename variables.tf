442675ba042aa0958a6541d2565d6ed16ca4124c
ThameeraSithnuwan <149369868+ThameeraSithnuwan@users.noreply.github.com>
2025-08-06 19:09:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,56 +1,15 @@
-variable "instance_type" {
-  description = "The EC2 instance type"
+variable "aws_region" {
+  description = "The AWS region to create the S3 bucket in"
   type        = string
-  default     = "t2.micro"
 }
 
-variable "instance_count" {
-  description = "The number of EC2 instances to create"
-  type        = number
-  default     = 1
-}
-
-variable "tags" {
-  description = "A map of tags to apply to resources"
-  type        = map(string)
-  default     = {
-    "Environment" = "dev"
-    "Project"     = "example"
-  }
-}
-
-variable "vpc_id" {
-  description = "vpc-id"
-  type= string
-  default="vpc-0fbad5b11bc87230b"
-}
-
-variable "key_name" {
-  description = "Key pair name"
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
-  default = "outroom"
 }
 
-variable "ec2_subnet_id" {
-  description = "EC2 subnet ID"
+variable "environment" {
+  description = "Environment name"
   type        = string
-  default = "subnet-01f3ef012620e408a"  
-}
-
-variable "enable_public_ip" {
-  description = "Enable public IP"
-  type        = bool
-  default     = true
-}
-
-variable "ec2_ami_id" {
-  description = "EC2 AMI ID"
-  type        = string
-  default     = "ami-0522ab6e1ddcc7055"  
-}
-
-variable "ec2_security_group_name" {
-  description = "ec2_security_group_name"
-  type        = string
-  default     = "Allow-SSH"  
+  default     = "dev"
 }
